c2be273236172c5cc00da76a1c579922bbaf9a5d
diegolins10 <dede140126>
2022-01-31 20:41:19 -0300

    a

--- FileDownload/Controllers/DownloadController.cs
+++ FileDownload/Controllers/DownloadController.cs
@@ -1,87 +1,37 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
 namespace FileDownload.Controllers
 {
-    public class DownloadController : Controller
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DownloadController : ControllerBase
     {
-        // GET: DownloadController
-        public ActionResult Index()
-        {
-            return View();
-        }
+        private readonly string filePath;
 
-        // GET: DownloadController/Details/5
-        public ActionResult Details(int id)
+        public DownloadController(string filePath)
         {
-            return View();
+            this.filePath = filePath;
         }
+        
 
-        // GET: DownloadController/Create
-        public ActionResult Create()
+        // GET: api/<DownloadController>
+        [HttpGet]
+        public FileContentResult Get()
         {
-            return View();
-        }
-
-        // POST: DownloadController/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
-        {
-            try
+            var data = System.IO.File.ReadAllBytes(filePath);
+            var result = new FileContentResult(data, filePath);
             {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
+                FileDownloadName = "File.csv";
+            };
+            return result;
         }
 
-        // GET: DownloadController/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: DownloadController/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: DownloadController/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: DownloadController/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
+      
     }
 }
