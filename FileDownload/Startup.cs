740eb08d1c377872edc087464c74ee12326f9302
diegolins10 <dede140126>
2022-01-31 22:30:25 -0300

    adicionando swagger

--- FileDownload/Startup.cs
+++ FileDownload/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,6 +27,12 @@ namespace FileDownload
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "FileDownload", Version = "v1" });
+            });
+
+            services.AddSingleton(System.IO.Path.Combine(System.IO.Directory.GetCurrentDirectory(), "File.csv"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,6 +41,8 @@ namespace FileDownload
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "FileDownload v1"));
             }
 
             app.UseHttpsRedirection();
