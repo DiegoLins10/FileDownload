8e0b9bc3aeb3b77e493ca37cca81ebb5162c3fb9
diegolins10 <dede140126>
2022-01-31 23:18:57 -0300

    arquivos estaticos

---
+++ FileDownload/StaticFile/main.js
@@ -0,0 +1,9 @@
+﻿
+// Função que chama o dowload quando clickado
+(function () {
+    $(document).ready(function () {
+        $(":button").click(function () {
+            window.open('https://localhost:5001/api/download', '_blank')
+        });
+    });
+})();
